81a63167adbfe01332ed3242f0052116082a5676
kinyth <kennethphu@gmail.com>
2022-09-27 23:27:27 -0700

    Switched up the story of the madlibs

--- Madlibs.java
+++ Madlibs.java
@@ -1,13 +1,18 @@
 import java.util.Scanner;
+
 public class Madlibs{
     public static void main(String[] args) {
         System.out.print("Here is your madlibs: ");
-        System.out.println("I went to the animal " + "<noun>" + ", the " + "<plural_noun>" + " and the " + "<number>" + " beasts were there.");
-    
-        Scanner sc = new Scanner(System.in);
-        String noun = sc.nextLine();
-        System.out.println("I went to the animal" + noun + ", the" + "<plural_noun>" + "and the" + "<number>" + "beasts were there.");
+        System.out.println("Once upon a time in a <adjective> land, a <noun> <adverb> grew.");
+        System.out.print("Now type in an adjective: ");
 
+        Scanner sc = new Scanner(System.in);
+        String adjective = sc.nextLine();
+        System.out.println("Once upon a time in a "+ adjective + " land, a <noun> <adverb> grew.");
+        
+        System.out.print("Now type in a noun: ");
+        String noun = sc.next();
+        
 
     }
 }
