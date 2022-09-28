776d5c2f65a57aaf37e156c0be841a4fe59410ed
kinyth <kennethphu@gmail.com>
2022-09-27 23:13:28 -0700

    Created the user input for noun

--- Madlibs.java
+++ Madlibs.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
-
-public static void main(String[] args) {
+public class Madlibs{
+    public static void main(String[] args) {
+        System.out.print("Here is your madlibs: ");
+        System.out.println("I went to the animal " + "<noun>" + ", the " + "<plural_noun>" + " and the " + "<number>" + " beasts were there.");
     
+        Scanner sc = new Scanner(System.in);
+        String noun = sc.nextLine();
+        System.out.println("I went to the animal" + noun + ", the" + "<plural_noun>" + "and the" + "<number>" + "beasts were there.");
+
+
+    }
 }
